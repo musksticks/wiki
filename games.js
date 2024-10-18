c50a3db77e8b73a4fc08ce13172bc8fdb59ad4b3
musksticks <62134419+musksticks@users.noreply.github.com>
2024-10-19 02:23:49 +1030

    Complete Rewrite/Rework
    Rewrote the code to make it save a fuck ton of space, also added a bunch of new shit such as stats, fixed some old issues aswell.

---
+++ games.js
@@ -0,0 +1,17 @@
+const base = "https://wiki.musk.ovh/Games/";
+const games = [
+    { title: "Escape From Tarkov", image: "eftbackground.jpg", url: "EFT" },
+    { title: "Company of Heroes 2", image: "coh2background.jpg", url: "COH2" },
+    { title: "Hearts of Iron 4", image: "hoi4.jpg", url: "HOI4" },
+    { title: "Valorant", image: "valorant.jpg", url: "Valorant" },
+    { title: "Minecraft", image: "minecraft.jpeg", url: "Minecraft" },
+    { title: "Unturned", image: "unturned.jpg", url: "Unturned" },
+    { title: "Dont Starve Together", image: "dontstarvetogether.jpg", url: "DST" },
+    { title: "Roblox", image: "roblox.jpg", url: "Roblox" },
+    { title: "League of Legends", image: "lol.jpg", url: "LOL" },
+    { title: "Stardew Valley", image: "stardewvalley.jpg", url: "StardewValley" },
+    { title: "Ark: Survival Evolved", image: "arksurvivelevolved.jpg", url: "ARK" },
+    { title: "Guilty Gear Strive", image: "ggs.jpg", url: "GGS" },
+    { title: "Metal Gear Rising: Revengeance", image: "mgr.jpg", url: "MGR" },
+    { title: "Five Nights at Mina's", image: "placeholder.jpg", url: "placeholder" }
+];
--- members.js
+++ members.js
@@ -1,12 +1,14 @@
+const base = "https://wiki.musk.ovh/Members/"
 const members = [
-    { title: "Funkysnails", url: "Members/Funkysnails/" },
-    { title: "GoldenMinass", url: "Members/GoldenMinass/" },
-    { title: "Lolzard", url: "Members/Lolzard/" },
-    { title: "MartP", url: "Members/MartP/" },
-    { title: "Musk", url: "Members/Musk/" },
-    { title: "Nick999", url: "Members/Nick999/" },
-    { title: "Nightowlofdoom", url: "Members/Nightowlofdoom/" },
-    { title: "Ratman", url: "Members/Ratman/" },
-    { title: "Rex_pimpwagen", url: "Members/Rex_pimpwagen/" },
-    { title: "Steelix", url: "Members/Steelix/" }
+    { title: "Funkysnails", url: base+"Funkysnails"},
+    { title: "GoldenMinass", url: base+"GoldenMinass" },
+    { title: "Lolzard", url: base+"Lolzard" },
+    { title: "MartP", url: base+"MartP" },
+    { title: "Musk", url: base+"Musk" },
+    { title: "Nick999", url: base+"Nick999" },
+    { title: "Nightowlofdoom", url: base+"Nightowlofdoom" },
+    { title: "Ratman", url: base+"Ratman" },
+    { title: "Rex_pimpwagen", url: base+"Rex_pimpwagen" },
+    { title: "Steelix", url: base+"Steelix" },
+    { title: "Medi", url: base+"Medi" },
 ];
